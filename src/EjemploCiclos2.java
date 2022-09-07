c9b43253e26e63e468d121e2b14b13c417820890
Ignacio Femenia <femeniax@gmail.com>
2022-09-07 19:08:38 -0300

    Create EjemploCiclos2.java

---
+++ src/EjemploCiclos2.java
@@ -0,0 +1,20 @@
+
+public class EjemploCiclos2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int contador = 0;
+		while (contador <= 10) {
+			int total = 0;
+			total = total + contador;
+			System.out.println(total);
+			contador++;
+			System.out.println(total);
+		}
+		for(int contador2 = 0 ; contador2 <=10 ; contador2++ ) {
+			System.out.println(contador);
+		}
+		
+	}
+
+}
