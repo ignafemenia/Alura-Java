a1e27fbf1a273bd016e5f48c46f34a7cc3f4622b
Ignacio Femenia <femeniax@gmail.com>
2022-09-07 18:49:40 -0300

    Create EjemploCiclos.java

---
+++ src/EjemploCiclos.java
@@ -0,0 +1,14 @@
+
+public class EjemploCiclos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int contador = 0;
+		while(contador<10) { //mientras que (condicion)
+			//ejecuta
+			System.out.println(contador);
+			contador = contador+1; //contador+=1; || contador++;
+		}
+	}
+
+}
