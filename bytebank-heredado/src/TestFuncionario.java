0b7d94032eb93fb4021b85dc175c155fbcff5a87
Ignacio Femenia <femeniax@gmail.com>
2022-09-13 18:38:55 -0300

    Funcionario y TestFuncionario

---
+++ bytebank-heredado/src/TestFuncionario.java
@@ -0,0 +1,11 @@
+
+public class TestFuncionario {
+
+	public static void main(String[] args) {
+		Funcionario diego = new Funcionario();
+		diego.setDocumento("Diego");
+		diego.setDocumento("215125215");
+		diego.setSalario(2000);
+		System.out.println(diego.getSalario());
+	}
+}
